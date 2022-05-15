f51730ef9e5d6920476a49b74b3c8cfab96aba2f
Ptora <cfison@gmail.com>
2022-05-15 20:11:15 +0100

    1

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
-#https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
+# terraform {
+#  backend "azurerm" {
+#     resource_group_name   = "learn"
+#     storage_account_name  = "learn_storage"
+#     container_name        = "learn_container"
+#     key                   = "network.tfstate"
+# }
+# }
 
 # Ctrl+K+C  - to comment out
 # Ctrl+K+U - to uncomment
