5611b722a7359bafdc57a122a38b7d85fe5e6d19
Ptora <cfison@gmail.com>
2022-05-15 19:20:59 +0100

    upd

--- main.tf
+++ main.tf
@@ -9,19 +9,3 @@ resource "azurerm_resource_group" "rg-ft-learn" {
   name     = "learn"
 
 }
-
-#Create a VNET
-resource "azurerm_virtual_network" "learn-vnet" {
-  address_space       = ["10.0.0.0/16"]
-  location            = "${azurerm_resource_group.rg-ft-learn.location}"
-  name                = "${azurerm_resource_group.rg-ft-learn.name}-vnet"
-  resource_group_name = "${azurerm_resource_group.rg-ft-learn.name}"
-  
-}
-
-#Create a NSG
-resource "azurerm_network_security_group" "learn-nsg" {
-  location            = "${azurerm_resource_group.rg-ft-learn.location}"
-    name                = "${azurerm_resource_group.rg-ft-learn.name}-nsg"
-  resource_group_name = "${azurerm_resource_group.rg-ft-learn.name}"  
-}
