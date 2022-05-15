3c90565512d5ad46fb4505b336488c83ab6465e2
Ptora <cfison@gmail.com>
2022-05-15 19:20:52 +0100

    upd

---
+++ network.tf
@@ -0,0 +1,20 @@
+#https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
+
+# Ctrl+K+C  - to comment out
+# Ctrl+K+U - to uncomment
+
+#Create a VNET
+resource "azurerm_virtual_network" "learn-vnet" {
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg-ft-learn.location
+  name                = "${azurerm_resource_group.rg-ft-learn.name}-vnet"
+  resource_group_name = azurerm_resource_group.rg-ft-learn.name
+
+}
+
+#Create a NSG
+resource "azurerm_network_security_group" "learn-nsg" {
+  location            = azurerm_resource_group.rg-ft-learn.location
+  name                = "${azurerm_resource_group.rg-ft-learn.name}-nsg"
+  resource_group_name = azurerm_resource_group.rg-ft-learn.name
+}
