68964ce91b0420d66616b714f1134e9083af9863
Ptora <cfison@gmail.com>
2022-05-15 19:03:33 +0100

    maintf

--- main.tf
+++ main.tf
@@ -4,44 +4,17 @@
 # Ctrl+K+U - to uncomment
 
 #Create a RG
-resource "azurerm_resource_group" "fisontech" {
+resource "azurerm_resource_group" "rg-ft-learn" {
   location = "ukwest"
-  name     = "fison"
+  name     = "rg-ft-learn"
 
 }
 
 #Create a VNET
 resource "azurerm_virtual_network" "fisontech" {
   address_space       = ["10.0.0.0/16"]
-  location            = "${azurerm_resource_group.fisontech.location}"
-  name                = "${azurerm_resource_group.fisontech.name}-vnet"
-  resource_group_name = "${azurerm_resource_group.fisontech.name}"
+  location            = "${azurerm_resource_group.rg-ft-learn.location}"
+  name                = "${azurerm_resource_group.rg-ft-learn.name}-vnet"
+  resource_group_name = "${azurerm_resource_group.rg-ft-learn.name}"
   
 }
-
-# #Create a VM
-# resource "azurerm_virtual_machine" "fisontech" {
-#     location = "value"
-#     name = "value"
-#     network_interface_ids = [ "value" ]
-#     resource_group_name = "value"
-#     vm_size = "value"
-#     storage_os_disk {
-#         create_option = "value"
-#         name = "value"
-#     }
-# }
-
-
-
-
-
-
-# #Create a Subnet
-# resource "azurerm_subnet" "name" {
-#     address_prefixes = [ "value" ]
-#     name = "value"
-#     resource_group_name = "value"
-#     virtual_network_name = "value"
-
-# }
